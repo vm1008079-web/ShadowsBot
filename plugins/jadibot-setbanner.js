67782aba8edb0f9bdb246668173bbe96f2130724
Ado <minexdt@gmail.com>
2025-07-08 12:24:57 -0600

    Commit #1 👻

---
+++ plugins/jadibot-setbanner.js
@@ -0,0 +1,41 @@
+import fs from 'fs'
+import path from 'path'
+
+const handler = async (m, { text, usedPrefix, command }) => {
+  const senderNumber = m.sender.replace(/[^0-9]/g, '')
+  const botPath = path.join('./JadiBots', senderNumber)
+  const configPath = path.join(botPath, 'config.json')
+
+  
+  const urlRegex = /(https?:\/\/[^\s]+(\.jpg|\.jpeg|\.png|\.webp))/i
+  const match = text.match(urlRegex)
+  if (!match) {
+    return m.reply(`> 📸 Usa así:\n*${usedPrefix + command} https://example.com/banner.jpg*, osea sube la imagen a un hosting de imágenes como catbox.moe, o simplemente utiliza .catbox respondiendo a la imagen q quieres de banner.`)
+  }
+
+  if (!fs.existsSync(botPath)) {
+    return m.reply('✧ Este comando es sólo para los sub bots. ')
+  }
+
+  const bannerURL = match[1]
+
+  try {
+    const config = fs.existsSync(configPath)
+      ? JSON.parse(fs.readFileSync(configPath))
+      : {}
+
+    config.banner = bannerURL
+
+    fs.writeFileSync(configPath, JSON.stringify(config, null, 2))
+    m.reply(`☁︎ Se cambió el banner correctamente a:\n${bannerURL}`)
+  } catch (e) {
+    console.error(e)
+    m.reply('❌ No se pudo guardar el banner.')
+  }
+}
+
+handler.help = ['setbanner']
+handler.tags= ['serbot']
+handler.command = /^setbanner$/i
+handler.owner = false
+export default handler
