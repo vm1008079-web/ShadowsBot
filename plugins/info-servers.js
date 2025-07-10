3ab5307b12daa6cffbbf0fdf00da545b90fb8534
Ado <minexdt@gmail.com>
2025-07-09 20:17:36 -0600

    Update info-servers.js

--- plugins/info-servers.js
+++ plugins/info-servers.js
@@ -13,32 +13,34 @@ let handler = async (m, { conn, command }) => {
 
   let info = `
 「🌿」✧─── ･ ｡ﾟ★: *.✦ .* :★. ───✧
-➤ 𝚆𝙴𝙻𝙲𝙾𝙼𝙴 𝚃𝙾 *SYA SURVIVALS*
-✦↷ ᴊᴜᴇɢᴀ ʏ ᴄᴏɴsᴛʀᴜɪʀ ᴀʟ ᴍᴀxɪᴍᴏ
+\`𝚆𝙴𝙻𝙲𝙾𝙼𝙴 𝚃𝙾 *SYA SURVIVALS*\`
+> ✦↷ \`ᴊᴜᴇɢᴀ ʏ ᴄᴏɴsᴛʀᴜɪʀ ᴀʟ ᴍᴀxɪᴍᴏ\`
 ✧─── ･ ｡ﾟ★: *.✦ .* :★. ───✧
 
-➤ Servidor 1 - MultiCraft
-┆🌐 Plataforma: MultiCraft Build & Mine
-┆⬇️ Descargar:
-┆  play.google.com/store/apps/details?id=com.multicraft.game
-┆🎮 Modo: Survival
-┆🔑 Código: *Z8TY2ANB*
-┆⏰ Estado: 🟢 24/7 ON
+\`Servidor 1 (Para Android)\`
+> Plataforma: MultiCraft Build & Mine
+> Descargar:
+> play.google.com/store/apps/details?id=com.multicraft.game
+\`Modo: Survival\`
+> Código: *Z8TY2ANB*
+\`Estado: 🟢 24/7 ON\`
 
-➤ Servidor 2 - Minecraft Java
-┆⚙️ Versión: *1.8.8*
-┆🌍 IP: *207.180.254.11:12005*
-┆🎮 Modo: Survival
-┆⏰ Estado: 🟢 Siempre ONLINE
+\`Servidor 2 (Para PC)\`
+> Versión: *1.8.8*
+> IP: *207.180.254.11:12005*
+\`Modo: Survival\`
+\`Estado: 🟢 Siempre ONLINE\`
 
 ✧─── ･ ｡ﾟ★: *.✦ .* :★. ───✧
-➤ Comunidad sin tóxicos y con buena onda
-➤ Únete al *SYA TEAM* 🐾
+> Comunidad sin tóxicos y con buena onda
+> Únete al *SYA TEAM* 🐾
 「🌿」✧─── ･ ｡ﾟ★: *.✦ .* :★. ───✧
 `.trim()
 
   await conn.sendMessage(m.chat, { text: info }, rcanal)
 }
 
-handler.command = ['servers']
+handler.help = ['servers']
+handler.tags = ['info']
+handler.command = ['servers', 'minecraft', 'mc']
 export default handler
