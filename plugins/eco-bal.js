a090384c6d1348c42fec0a03a79fe8ef108979b6
Ado <minexdt@gmail.com>
2025-07-11 13:16:18 -0600

    Actualizar eco-bal.js

---
+++ plugins/eco-bal.js
@@ -0,0 +1,31 @@
+import db from '../lib/database.js'
+
+let handler = async (m, { conn, usedPrefix }) => {
+  let who = m.mentionedJid?.[0] || m.quoted?.sender || m.sender
+  if (who == conn.user.jid) return m.react('✖️')
+  if (!(who in global.db.data.users)) return m.reply(`❐✐ El usuario no se encuentra en mi base de datos.`)
+
+  let user = global.db.data.users[who]
+  let total = (user.coin || 0) + (user.bank || 0)
+  let name = await conn.getName(who)
+
+  const texto = 
+`❐ ᥫ᭡ *ECONOMÍA DE USUARIO* ❀
+
+✎ Usuario: *${name}*
+☁︎ Dinero en mano: *${user.coin || 0} ${moneda}*
+☁︎ Dinero en banco: *${user.bank || 0} ${moneda}*
+☄︎ Total acumulado: *${total} ${moneda}*
+
+✐ Usa: *${usedPrefix}deposit* para proteger tu dinero.`
+
+  await conn.reply(m.chat, texto, m, rcanal)
+}
+
+handler.help = ['bal']
+handler.tags = ['eco']
+handler.command = ['bal', 'balance', 'bank']
+handler.register = true
+handler.group = true
+
+export default handler
