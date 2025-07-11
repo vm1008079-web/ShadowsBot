d5402771229907f1455b6c439244b25e642adc7d
Ado <minexdt@gmail.com>
2025-07-11 12:18:54 -0600

    Actualizar eco-unreg.js

--- plugins/eco-unreg.js
+++ plugins/eco-unreg.js
@@ -1,30 +1,13 @@
-import Database from '../lib/database.js'
-const db = new Database('./database.json')
+let handler = async (m, { conn, text }) => {
 
-const handler = async (m) => {
-  const userId = m.sender
+let user = global.db.data.users[m.sender]
 
-  // forzar última versión de base
-  db.load()
-  await new Promise(res => setTimeout(res, 100)) // tiempo para que se cargue
+user.registered = false
+return conn.reply(m.chat, `${emoji} Tu registro fue borrado de mi base de datos.`, m)
 
-  if (!db.data.users || !db.data.users[userId]) {
-    return m.reply(`☁︎ ✐ No estás registrado ✐ ☁︎\n\nUsa *.reg Nombre Edad* para registrarte.`)
-  }
-
-  delete db.data.users[userId]
-  db.save()
-  await new Promise(res => setTimeout(res, 100)) // tiempo para que termine de guardar
-
-  return m.reply(
-`☁︎ ✐ Registro eliminado correctamente ✐ ☁︎
-
-Ya no estás registrado.
-
-☄︎ Puedes volver a registrarte cuando quieras usando *.reg Nombre Edad* ☄︎`)
 }
-
 handler.help = ['unreg']
-handler.tags = ['eco']
-handler.command = ['unreg', 'delete', 'remove']
+handler.tags = ['rg']
+handler.command = ['unreg']
+handler.register = true
 export default handler
