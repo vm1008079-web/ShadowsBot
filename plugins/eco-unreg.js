c16bb8481ccaf8163cb45a84049889d68f3146dc
Ado <minexdt@gmail.com>
2025-07-11 12:19:33 -0600

    Actualizar eco-unreg.js

--- plugins/eco-unreg.js
+++ plugins/eco-unreg.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text }) => {
 let user = global.db.data.users[m.sender]
 
 user.registered = false
-return conn.reply(m.chat, `${emoji} Tu registro fue borrado de mi base de datos.`, m)
+return conn.reply(m.chat, `> Tu registro fue borrado de mi base de datos.`, m)
 
 }
 handler.help = ['unreg']
