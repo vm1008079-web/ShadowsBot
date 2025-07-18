b3d14e98e483693f019e5369c39bb0e20faaa0b3
Ado <minexdt@gmail.com>
2025-07-18 07:40:02 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -21,8 +21,7 @@ global.sessions = 'Sessions'
 global.jadi = 'JadiBots'
 global.yukiJadibts = true
 
-global.namecanal = ' ‍ ‍ ‍ ‍ ‍
-່ ‍ '
+global.namecanal = '❀ Michi Wa ✦'
 global.idcanal = '120363403739366547@newsletter'
 global.canal = 'https://whatsapp.com/channel/0029Vb5pM031CYoMvQi2I02D'
 global.canalreg = '120363402895449162@newsletter'
