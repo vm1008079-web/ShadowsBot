e3b6515df56c68a672763f5a87727e4cfccc72b1
Ado <minexdt@gmail.com>
2025-07-28 08:21:46 -0600

    Actualizar sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -1,69 +1,49 @@
-import { sticker } from '../lib/sticker.js';
-import uploadFile from '../lib/uploadFile.js';
-import uploadImage from '../lib/uploadImage.js';
-import { webp2png } from '../lib/webp2mp4.js';
-
-let handler = async (m, { conn, args }) => {
-  let stiker = false;
-
+import { sticker } from '../lib/sticker.js'
+import uploadFile from '../lib/uploadFile.js'
+import uploadImage from '../lib/uploadImage.js'
+import { webp2png } from '../lib/webp2mp4.js'
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+  let stiker = false
+  let username = conn.getName(m.sender)
   try {
-    let q = m.quoted ? m.quoted : m;
-    let mime = (q.msg || q).mimetype || q.mediaType || '';
 
-    if (/webp|image|video/.test(mime)) {
-      if (/video/.test(mime) && (q.msg || q).seconds > 15) {
-        return m.reply('🏵️ El video no puede durar más de 15 segundos.');
-      }
-
-      let img = await q.download?.();
-      if (!img) return conn.reply(m.chat, '✦ Envía una imagen o video para hacer un sticker.', m);
-
-      let out;
-      const texto1 = '✧ 𝐒𝐭𝐢𝐜𝐤𝐞𝐫𝐬 𝐁𝐘 𝐌𝐢𝐜𝐡𝐢 - 𝐈𝐀✧';
-      const texto2 = await conn.getName(m.sender);
-
+    let q = m.quoted ? m.quoted : m
+    let mime = (q.msg || q).mimetype || q.mediaType || ''
+    if (/webp|image|video/g.test(mime)) {
+      let img = await q.download?.()
+      if (!img) return m.reply(`☁️ Responde a una *Imagen* o *Vídeo.*`)
+      let out
       try {
-        stiker = await sticker(img, false, texto1, texto2);
+        stiker = await sticker(img, false, global.packname, global.author)
       } catch (e) {
-        console.error(e);
+        console.error(e)
       } finally {
         if (!stiker) {
-          if (/webp/.test(mime)) out = await webp2png(img);
-          else if (/image/.test(mime)) out = await uploadImage(img);
-          else if (/video/.test(mime)) out = await uploadFile(img);
-          if (typeof out !== 'string') out = await uploadImage(img);
-          stiker = await sticker(false, out, texto1, texto2);
+          if (/webp/g.test(mime)) out = await webp2png(img)
+          else if (/image/g.test(mime)) out = await uploadImage(img)
+          else if (/video/g.test(mime)) out = await uploadFile(img)
+          if (typeof out !== 'string') out = await uploadImage(img)
+          stiker = await sticker(false, out, global.packname, global.author)
         }
       }
-
     } else if (args[0]) {
-      if (isUrl(args[0])) {
-        const texto1 = '✧ 𝐒𝐭𝐢𝐜𝐤𝐞𝐫𝐬 𝐁𝐘 𝐌𝐢𝐜𝐡𝐢 - 𝐈𝐀 ✧';
-        const texto2 = await conn.getName(m.sender);
-        stiker = await sticker(false, args[0], texto1, texto2);
-      } else {
-        return m.reply('🏵️ El Link es incorrecto.');
-      }
+      if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author)
+      else return m.reply('La *Url* es invalida')
     }
-
   } catch (e) {
-    // console.error(e);
-    if (!stiker) stiker = e;
+    console.error(e)
+    if (!stiker) stiker = e
   } finally {
-    if (stiker) {
-      return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m);
-    } else {
-      return conn.reply(m.chat, '🏵️ Envía una imagen o video para hacer un sticker.', m);
-    }
+    if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
+    else return m.reply('☁️ Responde a una *Imagen* o *Vídeo.*')
   }
-};
+}
+handler.help = ['sticker']
+handler.tags = ['sticker']
+handler.command = ['s', 'sticker', 'stiker'] 
 
-handler.help = ['sticker'];
-handler.tags = ['sticker'];
-handler.command = ['s', 'sticker', 'stiker'];
-
-export default handler;
+export default handler
 
 const isUrl = (text) => {
-  return /^https?:\/\/.+\.(jpg|jpeg|png|gif)$/i.test(text);
-};
+  return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'))
+}
