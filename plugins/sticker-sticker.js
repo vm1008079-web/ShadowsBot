b0e23360d492f4f3a149fccca9373562b62996ed
Ado <minexdt@gmail.com>
2025-07-29 17:15:32 -0600

    Actualizar sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -1,4 +1,72 @@
-import { sticker } from '../lib/sticker.js'
+import fs from 'fs'
+import path from 'path'
+import crypto from 'crypto'
+import { spawn } from 'child_process'
+import fluent_ffmpeg from 'fluent-ffmpeg'
+import fetch from 'node-fetch'
+import { fileTypeFromBuffer } from 'file-type'
+import webp from 'node-webpmux'
+
+const tmp = path.join(process.cwd(), 'tmp')
+if (!fs.existsSync(tmp)) fs.mkdirSync(tmp)
+
+async function addExif(webpSticker, packname, author, categories = [''], extra = {}) {
+  const img = new webp.Image()
+  const stickerPackId = crypto.randomBytes(32).toString('hex')
+  const json = {
+    'sticker-pack-id': stickerPackId,
+    'sticker-pack-name': packname,
+    'sticker-pack-publisher': author,
+    'emojis': categories,
+    ...extra
+  }
+  const exifAttr = Buffer.from([
+    0x49, 0x49, 0x2A, 0x00, 0x08, 0x00, 0x00, 0x00,
+    0x01, 0x00, 0x41, 0x57, 0x07, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x16, 0x00, 0x00, 0x00
+  ])
+  const jsonBuffer = Buffer.from(JSON.stringify(json), 'utf8')
+  const exif = Buffer.concat([exifAttr, jsonBuffer])
+  exif.writeUIntLE(jsonBuffer.length, 14, 4)
+  await img.load(webpSticker)
+  img.exif = exif
+  return await img.save(null)
+}
+
+async function sticker(img, url, packname, author) {
+  if (url) {
+    let res = await fetch(url)
+    if (res.status !== 200) throw await res.text()
+    img = await res.buffer()
+  }
+  const type = await fileTypeFromBuffer(img) || { mime: 'application/octet-stream', ext: 'bin' }
+  if (type.ext === 'bin') throw new Error('Tipo de archivo inválido')
+
+  const tmpFile = path.join(tmp, `${Date.now()}.${type.ext}`)
+  const outFile = `${tmpFile}.webp`
+  await fs.promises.writeFile(tmpFile, img)
+
+  await new Promise((resolve, reject) => {
+    const ff = /video/i.test(type.mime)
+      ? fluent_ffmpeg(tmpFile).inputFormat(type.ext)
+      : fluent_ffmpeg(tmpFile).input(tmpFile)
+
+    ff.addOutputOptions([
+      `-vcodec`, `libwebp`, `-vf`,
+      `scale='min(512,iw)':min'(512,ih)':force_original_aspect_ratio=decrease,fps=15, pad=512:512:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`
+    ])
+      .toFormat('webp')
+      .save(outFile)
+      .on('error', reject)
+      .on('end', resolve)
+  })
+
+  const buffer = await fs.promises.readFile(outFile)
+  fs.promises.unlink(tmpFile).catch(() => {})
+  fs.promises.unlink(outFile).catch(() => {})
+
+  return await addExif(buffer, packname, author)
+}
 
 const handler = async (m, { conn }) => {
   const q = m.quoted ? m.quoted : m
