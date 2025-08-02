5e1035fb569644691df7212184793b6e854f8be4
Ado <minexdt@gmail.com>
2025-08-02 11:29:24 -0600

    Actualizar fun-messi.js

--- plugins/fun-messi.js
+++ plugins/fun-messi.js
@@ -3,40 +3,27 @@ import axios from "axios";
 const MESSI_JSON_URL = `https://raw.githubusercontent.com/davidprospero123/api-anime/main/BOT-JSON/Messi.json`;
 
 let handler = async (m, { conn, text, command }) => {
-    let res = (await axios.get(MESSI_JSON_URL)).data;
-    let url = res[Math.floor(Math.random() * res.length)];
-
-    // Detectar si responde a una imagen del bot con "siguiente"
-    if (m.quoted && m.quoted.message && /Messi/i.test(m.quoted.text || '') && /siguiente/i.test(m.text)) {
-        await conn.sendMessage(
-            m.chat,
-            {
-                image: { url },
-                caption: "*Messi*\n\n📸 Responde con *Siguiente* para ver otra",
-                viewOnce: true
-            },
-            { quoted: m }
-        );
+    // Si NO es el comando .messi y NO es un "siguiente", no hacemos nada
+    if (!/^messi$/i.test(command) && !(m.quoted && /Messi/i.test(m.quoted.text || '') && /siguiente/i.test(text))) {
         return;
     }
 
-    // Comando inicial .messi
-    if (command === 'messi') {
-        await conn.sendMessage(
-            m.chat,
-            {
-                image: { url },
-                caption: "*Messi*\n\n📸 Responde con *Siguiente* para ver otra",
-                viewOnce: true
-            },
-            { quoted: m }
-        );
-    }
+    let res = (await axios.get(MESSI_JSON_URL)).data;
+    let url = res[Math.floor(Math.random() * res.length)];
+
+    await conn.sendMessage(
+        m.chat,
+        {
+            image: { url },
+            caption: "*Messi*\n\n📸 Responde con *Siguiente* para ver otra",
+            viewOnce: true
+        },
+        { quoted: m }
+    );
 };
 
 handler.help = ['messi'];
 handler.tags = ['fun'];
-handler.customPrefix = /^siguiente$/i; // Detecta si el texto es "siguiente"
 handler.command = /^messi$/i;
 
 export default handler;
