c792e943b8c733450e18e99b668f83685e61e391
Ado <minexdt@gmail.com>
2025-08-02 11:22:46 -0600

    Crear fun-messi.js

---
+++ plugins/fun-messi.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+let handler = async (m, { conn, usedPrefix, command }) => {
+    let res = (await axios.get(`https://raw.githubusercontent.com/davidprospero123/api-anime/main/BOT-JSON/Messi.json`)).data;
+    let url = res[Math.floor(Math.random() * res.length)];
+
+    const buttons = [
+        {
+            buttonId: `.messi`,
+            buttonText: { displayText: "⚽ Ver más" },
+            type: 1
+        }
+    ];
+
+    await conn.sendMessage(
+        m.chat,
+        {
+            image: { url },
+            caption: "*Messi*",
+            buttons: buttons,
+            viewOnce: true
+        },
+        { quoted: m }
+    );
+};
+
+handler.help = ['messi'];
+handler.tags = ['fun'];
+handler.command = /^(messi)$/i;
+
+export default handler;
