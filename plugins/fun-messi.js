3b7a44eb4e745d309f9424b93ce3ab0f10b986ce
Ado <minexdt@gmail.com>
2025-08-03 10:00:28 -0600

    Actualizar fun-messi.js

--- plugins/fun-messi.js
+++ plugins/fun-messi.js
@@ -1,36 +1,39 @@
-import axios from "axios";
+import axios from "axios"
 
-const MESSI_JSON_URL = `https://raw.githubusercontent.com/davidprospero123/api-anime/main/BOT-JSON/Messi.json`;
-const MESSI_CONTEXT = new Set(); // IDs de mensajes enviados por el bot con Messi
+const MESSI_JSON_URL = `https://raw.githubusercontent.com/davidprospero123/api-anime/main/BOT-JSON/Messi.json`
+const MESSI_CONTEXT = new Set() // IDs de mensajes enviados por el bot con Messi
 
 let handler = async (m, { conn, text, command }) => {
-    const isMessiCommand = /^messi$/i.test(command);
-    const isNext = /^(siguiente|otra|ver más|mas)$/i.test(text);
-    const isReplyToMessi = m.quoted && MESSI_CONTEXT.has(m.quoted.id);
+  const isMessiCommand = /^messi$/i.test(command)
+  const isNext = /^(siguiente|otra|ver más|mas)$/i.test(text)
+  const isReplyToMessi = m.quoted && MESSI_CONTEXT.has(m.quoted.id)
 
-    if (!isMessiCommand && !(isNext && isReplyToMessi)) return;
+  if (!isMessiCommand && !(isNext && isReplyToMessi)) return
 
-    let res = (await axios.get(MESSI_JSON_URL)).data;
-    let url = res[Math.floor(Math.random() * res.length)];
+  let res = (await axios.get(MESSI_JSON_URL)).data
+  let url = res[Math.floor(Math.random() * res.length)]
 
-    let sentMsg = await conn.sendMessage(
-        m.chat,
-        {
-            image: { url },
-            caption: "*Aquí tienes*"
-        },
-        { quoted: m }
-    );
+  let sentMsg = await conn.sendMessage(
+    m.chat,
+    {
+      image: { url },
+      caption: "*Aquí tienes*",
+      contextInfo: {
+        ...global.rcanal
+      }
+    },
+    { quoted: m }
+  )
 
-    // Guardamos el ID del mensaje por si responden con "siguiente"
-    if (sentMsg.key && sentMsg.key.id) {
-        MESSI_CONTEXT.add(sentMsg.key.id);
-        setTimeout(() => MESSI_CONTEXT.delete(sentMsg.key.id), 60 * 1000); // Se limpia en 1 min
-    }
-};
+  // Guardamos el ID del mensaje por si responden con "siguiente"
+  if (sentMsg.key && sentMsg.key.id) {
+    MESSI_CONTEXT.add(sentMsg.key.id)
+    setTimeout(() => MESSI_CONTEXT.delete(sentMsg.key.id), 60 * 1000)
+  }
+}
 
-handler.help = ['messi'];
-handler.tags = ['fun'];
-handler.command = /^messi$/i;
+handler.help = ['messi']
+handler.tags = ['fun']
+handler.command = /^messi$/i
 
-export default handler;
+export default handler
