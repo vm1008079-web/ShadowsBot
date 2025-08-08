bef876634992442bcae305b22ccdba4b2b83aeb7
Ado <minexdt@gmail.com>
2025-08-07 21:04:09 -0600

    Crear tools-channel-id.js

---
+++ plugins/tools-channel-id.js
@@ -0,0 +1,19 @@
+import { generateWAMessageFromContent } from '@adiwajshing/baileys'
+
+let handler = async (m, { text, conn }) => {
+  if (!text) return m.reply('📎 Por favor proporciona el enlace.')
+  if (!text.includes('https://whatsapp.com/channel/')) return m.reply('❗ Enlace no válido.')
+
+  let result = text.split('https://whatsapp.com/channel/')[1]
+  let res = await conn.newsletterMetadata('invite', result)
+
+  let teks = `🆔 *ID:* ${res.id}\n📛 *Nombre:* ${res.name}\n👥 *Suscriptores:* ${res.subscribers}\n📶 *Estado:* ${res.state}\n✅ *Verificado:* ${res.verification === 'VERIFIED' ? 'Sí' : 'No'}`
+
+  await m.reply(teks)
+}
+
+handler.help = handler.command = ['channel-id']
+handler.tags = ['tools']
+handler.limit = true
+
+export default handler
