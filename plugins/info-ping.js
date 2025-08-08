7f8655e3961ba8a92172a0f322a3df7a0b623bcc
Ado <minexdt@gmail.com>
2025-08-07 21:42:11 -0600

    Actualizar info-ping.js

--- plugins/info-ping.js
+++ plugins/info-ping.js
@@ -12,6 +12,6 @@ let handler = async (m, { conn }) => {
 };
 handler.help = ["ping"];
 handler.tags = ["info"];
-handler.command = ["ping", "speed"];
+handler.command = ["ping", "p"];
 
 export default handler;
