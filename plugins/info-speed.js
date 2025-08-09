a82c46948b30d52f8963a6a84fa407b7ef87d061
Ado <minexdt@gmail.com>
2025-08-09 14:52:21 -0600

    Actualizar info-speed.js

--- plugins/info-speed.js
+++ plugins/info-speed.js
@@ -20,7 +20,7 @@ let chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats
 let groups = Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats && !chat.metadata?.read_only && !chat.metadata?.announce).map(v => v[0])
 
 
-let texto = `${emoji} *${packname}*
+let texto = `
 🚀 *Velocidad:*
 → ${latensi.toFixed(4)}
 
