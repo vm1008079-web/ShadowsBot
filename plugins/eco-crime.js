71753b7f71c9eb795c9ac92975d569a1a6a573a4
Ado <minexdt@gmail.com>
2025-08-11 16:19:30 -0600

    Actualizar eco-crime.js

--- plugins/eco-crime.js
+++ plugins/eco-crime.js
@@ -23,58 +23,73 @@ let handler = async (m, { conn, text, command, usedPrefix }) => {
   let maxAmount = 50
   let amountTaken = Math.floor(Math.random() * (maxAmount - minAmount + 1)) + minAmount
 
-  const moneda = '💰'
+  
 
   const frases = {
-    exito: [
-      `✧ Le cagaste la vida a @${randomUserId.split("@")[0]} y le arrebataste *${amountTaken} ${moneda}*.`,
-      `⚠ Con un machetazo y un susto le quitaste *${amountTaken} ${moneda}* a @${randomUserId.split("@")[0]}.`,
-      `❀ Le metiste una buena trompada y le sacaste *${amountTaken} ${moneda}*.`,
-      `☄︎ Partiste la madre a @${randomUserId.split("@")[0]} y saliste con *${amountTaken} ${moneda}*.`,
-      `🔪 Le diste piso y te robaste *${amountTaken} ${moneda}*, qué perrote.`,
-      `💸 Desvalijaste como todo un capo y te pelaste con *${amountTaken} ${moneda}*.`,
-      `💀 Atracaste en la esquina y saliste con *${amountTaken} ${moneda}*.`,
-      `😈 Le hiciste la 13-14 y le volaste *${amountTaken} ${moneda}*.`,
-      `🧨 Le explotaste la cartera y te largaste con *${amountTaken} ${moneda}*.`,
-      `🔥 No supo ni qué pedo y ya le habías quitado *${amountTaken} ${moneda}*.`,
-      `🐒 Lo agarraste de pendejo y le tumbaste *${amountTaken} ${moneda}*.`,
-      `🎭 Usaste máscara y todo, y asaltaste por *${amountTaken} ${moneda}*.`,
-      `🚬 Te le apareciste como sombra y lo dejaste sin *${amountTaken} ${moneda}*.`,
-      `🏃‍♂️ Le hiciste el pase mágico y le bajaste *${amountTaken} ${moneda}*.`,
-      `🧤 Le metiste la mano al bolsillo y ni cuenta se dio, *${amountTaken} ${moneda}*.`,
-      `🕶 Con estilo y facha te lo tranzaste por *${amountTaken} ${moneda}*.`
-    ],
-    atrapado: [
-      `⚠ Te cazaron y te quitaron *${amountTaken} ${moneda}* por mamón.`,
-      `❀ Policía te agarró y perdiste *${amountTaken} ${moneda}*, qué sad.`,
-      `✧ Te descubrieron y te robaron *${amountTaken} ${moneda}*, rata torpe.`,
-      `☄︎ Te pillaron con las manos en la masa y perdiste *${amountTaken} ${moneda}*.`,
-      `🚓 Te agarraron los tombos y te metieron la macana por robar *${amountTaken} ${moneda}*.`,
-      `👮‍♂️ La jura te tundió y te volaron *${amountTaken} ${moneda}*.`,
-      `🧱 Te metiste con los duros y te dejaron sin nada.`,
-      `🩻 Te cacharon en cámara y te reventaron por *${amountTaken} ${moneda}*.`,
-      `🥴 Saliste pa'l hospital y encima sin billete.`,
-      `🙃 Te tropezaste huyendo y te quitaron *${amountTaken} ${moneda}*.`,
-      `🥵 Se te cayó el botín en plena fuga, qué mamada.`,
-      `🚔 Llegó la patrulla y te bajaron con todo.`,
-      `💢 Una doña te agarró a bolsazos y perdiste *${amountTaken} ${moneda}*.`
-    ],
-    semi: [
-      `⚠ Lograste robar pero te vieron y solo agarraste *${amountTaken} ${moneda}*.`,
-      `❀ Hiciste un robo chueco y solo te quedaron *${amountTaken} ${moneda}*.`,
-      `✧ Le quitaste algo, pero te cacharon y solo te quedaron *${amountTaken} ${moneda}*.`,
-      `☄︎ Robaste a medias y sacaste *${amountTaken} ${moneda}*.`,
-      `🫥 Te tembló la mano pero igual agarraste *${amountTaken} ${moneda}*.`,
-      `🥷 Medio ninja fuiste y te llevaste *${amountTaken} ${moneda}*.`,
-      `😬 Lo hiciste todo nervioso y te escapaste con *${amountTaken} ${moneda}*.`,
-      `💨 Fuiste rápido pero no tanto, te llevaste *${amountTaken} ${moneda}*.`,
-      `🤕 Casi te revientan pero saliste con *${amountTaken} ${moneda}*.`,
-      `👟 Corriste como loco y apenas te quedó *${amountTaken} ${moneda}*.`,
-      `🐀 Te agarraste lo que pudiste y saliste huyendo.`,
-      `😅 Medio robo, medio susto, y *${amountTaken} ${moneda}* de ganancia.`,
-      `🤡 Estabas por lograrlo pero hiciste ruido y saliste con lo que pudiste.`
-    ]
-  }
+  exito: [
+    `✧ Le cagaste la vida a @${randomUserId.split("@")[0]} y le arrebataste *${amountTaken} ${moneda}*.`,
+    `⚠ Con un machetazo y un susto le quitaste *${amountTaken} ${moneda}* a @${randomUserId.split("@")[0]}.`,
+    `❀ Le metiste una buena trompada a @${randomUserId.split("@")[0]} y le sacaste *${amountTaken} ${moneda}*.`,
+    `☄︎ Partiste la madre a @${randomUserId.split("@")[0]} y saliste con *${amountTaken} ${moneda}*.`,
+    `🔪 Le diste piso a @${randomUserId.split("@")[0]} y te robaste *${amountTaken} ${moneda}*, qué perrote.`,
+    `💸 Desvalijaste a @${randomUserId.split("@")[0]} como todo un capo y te pelaste con *${amountTaken} ${moneda}*.`,
+    `💀 Atracaste a @${randomUserId.split("@")[0]} en la esquina y saliste con *${amountTaken} ${moneda}*.`,
+    `😈 Le hiciste la 13-14 a @${randomUserId.split("@")[0]} y le volaste *${amountTaken} ${moneda}*.`,
+    `🧨 Le explotaste la cartera a @${randomUserId.split("@")[0]} y te largaste con *${amountTaken} ${moneda}*.`,
+    `🔥 @${randomUserId.split("@")[0]} no supo ni qué pedo y ya le habías quitado *${amountTaken} ${moneda}*.`,
+    `🐒 Agarraste de pendejo a @${randomUserId.split("@")[0]} y le tumbaste *${amountTaken} ${moneda}*.`,
+    `🎭 Usaste máscara y todo para asaltar a @${randomUserId.split("@")[0]} por *${amountTaken} ${moneda}*.`,
+    `🚬 Te le apareciste como sombra a @${randomUserId.split("@")[0]} y lo dejaste sin *${amountTaken} ${moneda}*.`,
+    `🏃‍♂️ Le hiciste el pase mágico a @${randomUserId.split("@")[0]} y le bajaste *${amountTaken} ${moneda}*.`,
+    `🧤 Le metiste la mano al bolsillo a @${randomUserId.split("@")[0]} y ni cuenta se dio, *${amountTaken} ${moneda}*.`,
+    `🕶 Con estilo y facha te tranzaste a @${randomUserId.split("@")[0]} por *${amountTaken} ${moneda}*.`,
+    // Nuevas alaver pero claras
+    `💥 Le diste un batazo a @${randomUserId.split("@")[0]} y le volaron las ganas de vivir, pero te quedaste con *${amountTaken} ${moneda}*.`,
+    `🤯 Le bajaste hasta la dignidad a @${randomUserId.split("@")[0]} y encima te llevaste *${amountTaken} ${moneda}*.`,
+    `🪓 Le dejaste la cartera partida en dos a @${randomUserId.split("@")[0]} y saliste corriendo con *${amountTaken} ${moneda}*.`,
+    `🩸 Dejaste sangrando a @${randomUserId.split("@")[0]} pero con menos *${amountTaken} ${moneda}*.`
+  ],
+  atrapado: [
+    `⚠ Te cazaron y te quitaron *${amountTaken} ${moneda}* por mamón.`,
+    `❀ La policía te agarró y perdiste *${amountTaken} ${moneda}*, qué sad.`,
+    `✧ Te descubrieron robando y te quitaron *${amountTaken} ${moneda}*, rata torpe.`,
+    `☄︎ Te pillaron con las manos en la masa y perdiste *${amountTaken} ${moneda}*.`,
+    `🚓 Te agarraron los tombos y te metieron la macana por robar *${amountTaken} ${moneda}*.`,
+    `👮‍♂️ La jura te tundió y te volaron *${amountTaken} ${moneda}*.`,
+    `🧱 Te metiste con los duros y te dejaron sin nada.`,
+    `🩻 Te cacharon en cámara y te reventaron por *${amountTaken} ${moneda}*.`,
+    `🥴 Saliste pa'l hospital y encima sin billete.`,
+    `🙃 Te tropezaste huyendo y te quitaron *${amountTaken} ${moneda}*.`,
+    `🥵 Se te cayó el botín en plena fuga, qué mamada.`,
+    `🚔 Llegó la patrulla y te bajaron con todo.`,
+    `💢 Una doña te agarró a bolsazos y perdiste *${amountTaken} ${moneda}*.`,
+    // Nuevas alaver pero claras
+    `🪦 Te mandaron directo al suelo y encima te volaron *${amountTaken} ${moneda}*.`,
+    `🤕 Te dieron hasta con la chancla del 45 y perdiste *${amountTaken} ${moneda}*.`,
+    `🗿 Quedaste más tieso que estatua y sin *${amountTaken} ${moneda}*.`,
+    `🥶 Te congelaron de un macanazo y adiós *${amountTaken} ${moneda}*.`
+  ],
+  semi: [
+    `⚠ Lograste robar a @${randomUserId.split("@")[0]} pero te vieron y solo agarraste *${amountTaken} ${moneda}*.`,
+    `❀ Hiciste un robo chueco a @${randomUserId.split("@")[0]} y solo te quedaron *${amountTaken} ${moneda}*.`,
+    `✧ Le quitaste algo a @${randomUserId.split("@")[0]}, pero te cacharon y solo te quedaron *${amountTaken} ${moneda}*.`,
+    `☄︎ Robaste a medias a @${randomUserId.split("@")[0]} y sacaste *${amountTaken} ${moneda}*.`,
+    `🫥 Te tembló la mano robando a @${randomUserId.split("@")[0]} pero igual agarraste *${amountTaken} ${moneda}*.`,
+    `🥷 Medio ninja fuiste con @${randomUserId.split("@")[0]} y te llevaste *${amountTaken} ${moneda}*.`,
+    `😬 Lo hiciste todo nervioso con @${randomUserId.split("@")[0]} y te escapaste con *${amountTaken} ${moneda}*.`,
+    `💨 Fuiste rápido pero no tanto, le robaste a @${randomUserId.split("@")[0]} *${amountTaken} ${moneda}*.`,
+    `🤕 Casi te revientan robando a @${randomUserId.split("@")[0]} pero saliste con *${amountTaken} ${moneda}*.`,
+    `👟 Corriste como loco después de robar a @${randomUserId.split("@")[0]} y apenas te quedó *${amountTaken} ${moneda}*.`,
+    `🐀 Le agarraste a @${randomUserId.split("@")[0]} lo que pudiste y saliste huyendo.`,
+    `😅 Medio robo a @${randomUserId.split("@")[0]}, medio susto, y *${amountTaken} ${moneda}* de ganancia.`,
+    `🤡 Estabas por lograrlo con @${randomUserId.split("@")[0]} pero hiciste ruido y saliste con lo que pudiste.`,
+    // Nuevas alaver pero claras
+    `🩻 Te llevaste algo de @${randomUserId.split("@")[0]} pero quedaste cojeando con *${amountTaken} ${moneda}*.`,
+    `🥴 Casi la cagas con @${randomUserId.split("@")[0]} pero alcanzaste a agarrar *${amountTaken} ${moneda}*.`,
+    `🪃 El robo a @${randomUserId.split("@")[0]} se te devolvió pero te quedó *${amountTaken} ${moneda}*.`,
+    `💀 Le quitaste poquito a @${randomUserId.split("@")[0]} y casi quedas en el piso.`
+  ]
+}  
 
   let randomOption = Math.floor(Math.random() * 3)
 
