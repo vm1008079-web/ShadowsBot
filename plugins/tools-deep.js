d20c23572fab7658a00a133babe912a38a551ac4
Ado <minexdt@gmail.com>
2025-08-12 21:34:46 -0600

    Actualizar tools-deep.js

--- plugins/tools-deep.js
+++ plugins/tools-deep.js
@@ -22,6 +22,6 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 handler.help = ['deepseek']
 handler.tags = ['tools']
 handler.command = /^(deep|deepseek|deeps)$/i
-handler.register = true
+handler.register = false
 
 export default handler
