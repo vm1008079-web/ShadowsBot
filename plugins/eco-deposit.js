c2f0456bbc8c3314ef5d7cecdea31aae84106abf
Ado <minexdt@gmail.com>
2025-08-12 20:51:18 -0600

    Actualizar eco-deposit.js

--- plugins/eco-deposit.js
+++ plugins/eco-deposit.js
@@ -1,35 +1,42 @@
 import db from '../lib/database.js'
 
-let handler = async (m, { args }) => {
+let handler = async (m, { args, conn }) => {
   let user = global.db.data.users[m.sender]
 
-  if (!args[0]) return m.reply(`✧ Ingresa la cantidad de *${moneda}* que deseas depositar.`)
+  if (!args[0]) 
+    return conn.reply(m.chat, `⚠️ Ingresa la cantidad de *${moneda}* que deseas depositar.`, m, { ...global.rcanal })
 
   if (args[0] === 'all') {
     let count = parseInt(user.coin)
-    if (!count || count < 1) return m.reply(`❀ No tienes nada para depositar.`)
+    if (!count || count < 1) 
+      return conn.reply(m.chat, `❌ No tienes nada para depositar.`, m, { ...global.rcanal })
+
     user.coin -= count
     user.bank += count
-    await m.reply(`✿ Depositaste *${count} ${moneda}* en el banco. Ya no te lo pueden robar alv.`)
-    return
+
+    return conn.reply(m.chat, `✅ Depositaste *${count} ${moneda}* en el banco.\n💼 Ya no te lo pueden robar.`, m, { ...global.rcanal })
   }
 
-  if (isNaN(args[0])) return m.reply(`✦ Ingresa una cantidad válida.\nEjemplo: *#d 5000*`)
+  if (isNaN(args[0])) 
+    return conn.reply(m.chat, `⚠️ Ingresa una cantidad válida.\nEjemplo: *#deposit 5000*`, m, { ...global.rcanal })
 
   let count = parseInt(args[0])
-  if (!user.coin || user.coin < 1) return m.reply(`✧ No tienes nada en tu cartera.`)
-  if (user.coin < count) return m.reply(`❀ Solo tienes *${user.coin} ${moneda}* en la cartera.`)
+  if (!user.coin || user.coin < 1) 
+    return conn.reply(m.chat, `❌ No tienes dinero en la cartera.`, m, { ...global.rcanal })
+
+  if (user.coin < count) 
+    return conn.reply(m.chat, `⚠️ Solo tienes *${user.coin} ${moneda}* en la cartera.`, m, { ...global.rcanal })
 
   user.coin -= count
   user.bank += count
 
-  await m.reply(`✿ Depositaste *${count} ${moneda}* en el banco.`)
+  await conn.reply(m.chat, `✅ Depositaste *${count} ${moneda}* en el banco.`, m, { ...global.rcanal })
 }
 
 handler.help = ['depositar']
 handler.tags = ['eco']
 handler.command = ['deposit', 'depositar', 'dep', 'aguardar']
 handler.group = false
-handler.register = true
+handler.register = false
 
 export default handler
