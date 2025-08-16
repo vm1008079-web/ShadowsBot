fa86af003ef9e795f1985436bbad517f809cf3dd
Ado <minexdt@gmail.com>
2025-08-16 15:18:13 -0600

    Actualizar _AllFake.js

--- plugins/_AllFake.js
+++ plugins/_AllFake.js
@@ -3,7 +3,11 @@ import path from 'path'
 
 export async function before(m, { conn }) {
   try {
+
     let nombreBot = global.namebot || 'Bot'
+    let bannerFinal = 'https://raw.githubusercontent.com/AdonixServices/Files/main/1754310580366-xco6p1-1754310544013-6cc3a6.jpg'
+
+
     const botActual = conn.user?.jid?.split('@')[0].replace(/\D/g, '')
     const configPath = path.join('./JadiBots', botActual, 'config.json')
 
@@ -11,17 +15,33 @@ export async function before(m, { conn }) {
       try {
         const config = JSON.parse(fs.readFileSync(configPath))
         if (config.name) nombreBot = config.name
+        if (config.banner) bannerFinal = config.banner
       } catch (err) {
-        console.log('⚠️ No se pudo leer config del subbot:', err)
+        console.log('⚠️ No se pudo leer config del subbot en rcanal:', err)
       }
     }
 
+
+    const canales = [global.idcanal, global.idcanal2]
+    const newsletterJidRandom = canales[Math.floor(Math.random() * canales.length)]
+
+
     global.rcanal = {
-      key: { fromMe: false, participant: '0@s.whatsapp.net', ...(m.chat ? { remoteJid: m.chat } : {}) },
-      message: {
-        contactMessage: {
-          displayName: nombreBot,
-          vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:${nombreBot}\nORG:${global.author || 'Bot'};\nTEL;type=CELL;type=VOICE;waid=${botActual}:${botActual}\nEND:VCARD`
+      contextInfo: {
+        isForwarded: true,
+        forwardingScore: 1,
+        forwardedNewsletterMessageInfo: {
+          newsletterJid: newsletterJidRandom,
+          serverMessageId: 100,
+          newsletterName: nombreBot,
+        },
+        externalAdReply: {
+          title: nombreBot,
+          body: global.author,
+          thumbnailUrl: bannerFinal,
+          sourceUrl: 'myapiadonix.vercel.app',
+          mediaType: 1,
+          renderLargerThumbnail: false
         }
       }
     }
