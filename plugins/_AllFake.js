c5ff5cf7dce19ca7bd98e8d8c2e06964434eaa2d
Ado <minexdt@gmail.com>
2025-08-18 18:06:12 -0600

    Actualizar _AllFake.js

--- plugins/_AllFake.js
+++ plugins/_AllFake.js
@@ -3,10 +3,8 @@ import path from 'path'
 
 export async function before(m, { conn }) {
   try {
-
     let nombreBot = global.namebot || 'Bot'
     let bannerFinal = 'https://raw.githubusercontent.com/AdonixServices/Files/main/1754310580366-xco6p1-1754310544013-6cc3a6.jpg'
-
 
     const botActual = conn.user?.jid?.split('@')[0].replace(/\D/g, '')
     const configPath = path.join('./JadiBots', botActual, 'config.json')
@@ -21,14 +19,15 @@ export async function before(m, { conn }) {
       }
     }
 
-
     const canales = [global.idcanal, global.idcanal2]
     const newsletterJidRandom = canales[Math.floor(Math.random() * canales.length)]
 
+    
+    const isForwardedRandom = Math.random() < 0.5
 
     global.rcanal = {
       contextInfo: {
-        isForwarded: true,
+        isForwarded: isForwardedRandom,
         forwardingScore: 1,
         forwardedNewsletterMessageInfo: {
           newsletterJid: newsletterJidRandom,
