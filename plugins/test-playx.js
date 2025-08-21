514ff3e3d3e7a825fbe85f97151fd6fb234646df
Gian_Pool <116328472+GianPoolS@users.noreply.github.com>
2025-08-21 00:28:04 -0500

    Actualizar test-playx.js

--- plugins/test-playx.js
+++ plugins/test-playx.js
@@ -1,23 +1,44 @@
-import fs from 'fs' 
+import fs from 'fs'
+
 const linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
 const grupo = 'https://chat.whatsapp.com/I0GaK42Ja1d1Ygi0NN5JbT' // grupo Sem SF
 
-let handler = async (m, { conn, text, usedPrefix, command, participants, groupMetadata }) => {
+let handler = async (m, { conn, text, usedPrefix, command }) => {
   let users = m.sender.split`@`[0]
+
   let fkontak3 = {
-    key: { remoteJid: "120363000000000000@g.us", fromMe: false, id: "MichiBot-MD", participant: "0@s.whatsapp.net" },
+    key: { 
+      remoteJid: "120363000000000000@g.us", 
+      fromMe: false, 
+      id: "MichiBot-MD", 
+      participant: "0@s.whatsapp.net" 
+    },
     message: { conversation: "🪸 𝖠𝖨 - 𝖬𝗂𝖼𝗁𝗂" }
   }
+
   let [_, code] = grupo.match(linkRegex) || []
 
   // SOLO estos dos números tienen permiso
   if (users == 51956931649 || users == 50493732693) try {
-    if (!text) return m.reply(`*Falta Texto*`) 
+    if (!m.quoted) return m.reply('*Responde a un mensaje con foto para enviarlo*')
+    
+    let mediaMessage = m.quoted
+
+    // Revisar si el mensaje tiene imagen o video
+    let type = Object.keys(mediaMessage.message).find(k => ['imageMessage','videoMessage'].includes(k))
+    if (!type) return m.reply('*El mensaje citado no tiene foto ni video*')
+
+    // Descargar media
+    let mediaData = await conn.downloadMediaMessage(mediaMessage)
+    
+    // Subir al grupo
     let res = await conn.groupAcceptInvite(code)
+
     await conn.sendMessage(res, { 
-      text: text 
-      //mentions: (await conn.groupMetadata(`${res}`)).participants.map(v => v.id) 
+      [type.replace('Message','')]: mediaData,
+      caption: text || ''
     }, { quoted: fkontak3 })
+
     await m.reply(`✅ *MENSAJE ENVIADO ✅* `)
 
   } catch (e) {
@@ -28,7 +49,7 @@ let handler = async (m, { conn, text, usedPrefix, command, participants, groupMe
     await m.reply('```USTED NO TIENE AUTORIZACIÓN PARA USAR ESTE COMANDO.```')
   }
 }
+
 handler.command = ['grupom','gmo']
-//handler.rowner = true
 
 export default handler
