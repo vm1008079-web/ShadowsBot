7fc7c8b28759d673af725ac9e7e039f8a44d8c69
Ado <minexdt@gmail.com>
2025-08-21 21:33:12 -0600

    Actualizar _AllFake.js

--- plugins/_AllFake.js
+++ plugins/_AllFake.js
@@ -4,7 +4,6 @@ import path from 'path'
 export async function before(m, { conn }) {
   try {
     let nombreBot = global.namebot || 'Bot'
-    let bannerFinal = 'https://raw.githubusercontent.com/AdonixServices/Files/main/1754310580366-xco6p1-1754310544013-6cc3a6.jpg'
 
     const botActual = conn.user?.jid?.split('@')[0].replace(/\D/g, '')
     const configPath = path.join('./JadiBots', botActual, 'config.json')
@@ -13,7 +12,6 @@ export async function before(m, { conn }) {
       try {
         const config = JSON.parse(fs.readFileSync(configPath))
         if (config.name) nombreBot = config.name
-        if (config.banner) bannerFinal = config.banner
       } catch (err) {
         console.log('⚠️ No se pudo leer config del subbot en rcanal:', err)
       }
@@ -21,7 +19,6 @@ export async function before(m, { conn }) {
 
     const canales = [global.idcanal, global.idcanal2]
     const newsletterJidRandom = canales[Math.floor(Math.random() * canales.length)]
-
 
     const isForwardedRandom = Math.random() < 0.5
 
