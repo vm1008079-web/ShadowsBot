ed23ce9066a915657996dbaa2d514be31b2753da
Ado <minexdt@gmail.com>
2025-08-21 21:31:58 -0600

    Actualizar _AllFake.js

--- plugins/_AllFake.js
+++ plugins/_AllFake.js
@@ -4,6 +4,7 @@ import path from 'path'
 export async function before(m, { conn }) {
   try {
     let nombreBot = global.namebot || 'Bot'
+    let bannerFinal = 'https://raw.githubusercontent.com/AdonixServices/Files/main/1754310580366-xco6p1-1754310544013-6cc3a6.jpg'
 
     const botActual = conn.user?.jid?.split('@')[0].replace(/\D/g, '')
     const configPath = path.join('./JadiBots', botActual, 'config.json')
@@ -12,6 +13,7 @@ export async function before(m, { conn }) {
       try {
         const config = JSON.parse(fs.readFileSync(configPath))
         if (config.name) nombreBot = config.name
+        if (config.banner) bannerFinal = config.banner
       } catch (err) {
         console.log('⚠️ No se pudo leer config del subbot en rcanal:', err)
       }
@@ -20,15 +22,17 @@ export async function before(m, { conn }) {
     const canales = [global.idcanal, global.idcanal2]
     const newsletterJidRandom = canales[Math.floor(Math.random() * canales.length)]
 
-    const isForwardedRandom = true  // siempre aparece como reenviado
+
+    const isForwardedRandom = Math.random() < 0.5
 
     global.rcanal = {
       contextInfo: {
         isForwarded: isForwardedRandom,
         forwardingScore: 1,
-        forwarded: {
-          senderJid: newsletterJidRandom,
-          participant: newsletterJidRandom,
+        forwardedNewsletterMessageInfo: {
+          newsletterJid: newsletterJidRandom,
+          serverMessageId: 100,
+          newsletterName: nombreBot
         }
       }
     }
