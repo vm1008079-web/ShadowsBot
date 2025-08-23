5fe5b8f97c7e31348cf1eb7b79f331ddb5fc2056
Ado <minexdt@gmail.com>
2025-08-22 18:02:06 -0600

    Actualizar _AllFake.js

--- plugins/_AllFake.js
+++ plugins/_AllFake.js
@@ -3,7 +3,10 @@ import path from 'path'
 
 export async function before(m, { conn }) {
   try {
+
     let nombreBot = global.namebot || 'Bot'
+    let bannerFinal = 'https://raw.githubusercontent.com/AdonixServices/Files/main/1754310580366-xco6p1-1754310544013-6cc3a6.jpg'
+
 
     const botActual = conn.user?.jid?.split('@')[0].replace(/\D/g, '')
     const configPath = path.join('./JadiBots', botActual, 'config.json')
@@ -12,15 +15,17 @@ export async function before(m, { conn }) {
       try {
         const config = JSON.parse(fs.readFileSync(configPath))
         if (config.name) nombreBot = config.name
+        if (config.banner) bannerFinal = config.banner
       } catch (err) {
         console.log('⚠️ No se pudo leer config del subbot en rcanal:', err)
       }
     }
 
+
     const canales = [global.idcanal, global.idcanal2]
     const newsletterJidRandom = canales[Math.floor(Math.random() * canales.length)]
 
-    
+
     global.rcanal = {
       contextInfo: {
         isForwarded: true,
@@ -28,7 +33,15 @@ export async function before(m, { conn }) {
         forwardedNewsletterMessageInfo: {
           newsletterJid: newsletterJidRandom,
           serverMessageId: 100,
-          newsletterName: nombreBot
+          newsletterName: nombreBot,
+        },
+        externalAdReply: {
+          title: nombreBot,
+          body: global.author,
+          thumbnailUrl: bannerFinal,
+          sourceUrl: 'myapiadonix.vercel.app',
+          mediaType: 1,
+          renderLargerThumbnail: false
         }
       }
     }
