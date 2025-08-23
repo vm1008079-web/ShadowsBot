0efed2fa5cebf12b8eac031e0c0dfd86fa261006
Ado <minexdt@gmail.com>
2025-08-22 18:08:13 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ global.yukiJadibts = true
 
 global.namecanal = '❇️'
 global.idcanal = '120363403739366547@newsletter'
-global.idcanal2 = '120363402159669836@newsletter'
+global.idcanal2 = '120363403739366547@newsletter'
 global.canal = 'https://whatsapp.com/channel/0029Vb5pM031CYoMvQi2I02D'
 global.canalreg = '120363402895449162@newsletter'
 
