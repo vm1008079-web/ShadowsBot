85bb222ff1c42c761defd239ef02810d3d393507
Ado <minexdt@gmail.com>
2025-08-23 21:03:32 -0600

    Actualizar grupo-unbanchat.js

--- plugins/grupo-unbanchat.js
+++ plugins/grupo-unbanchat.js
@@ -1,10 +1,22 @@
-let handler = async (m, { conn, isAdmin, isROwner} ) => {
-    if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
-    global.db.data.chats[m.chat].isBanned = false
-    m.reply('🦞 Bot activo en este grupo.')   
+import fs from 'fs'
+import path from 'path'
+
+let handler = async (m, { conn, isAdmin, isROwner }) => {
+  const senderNumber = m.sender.replace(/[^0-9]/g, '')
+  const botPath = path.join('./JadiBots', senderNumber)
+
+  
+  if (!(isAdmin || isROwner || fs.existsSync(botPath))) {
+    return m.reply('❌ No tienes permisos para usar este comando. Solo admins, owners o subbots pueden hacerlo.')
+  }
+
+  global.db.data.chats[m.chat].isBanned = false
+  m.reply('🧃 Bot desbaneado en este grupo.')
 }
+
 handler.help = ['desbanearbot']
 handler.tags = ['group']
 handler.command = ['desbanearbot', 'unbanchat']
 handler.group = true 
+
 export default handler
