d3464b1b992998c8c1cd13bde7b1935cb9f6f0ab
Ado <minexdt@gmail.com>
2025-08-23 20:46:21 -0600

    Actualizar grupo-banchat.js

--- plugins/grupo-banchat.js
+++ plugins/grupo-banchat.js
@@ -1,10 +1,22 @@
+import fs from 'fs'
+import path from 'path'
+
 let handler = async (m, { conn, isAdmin, isROwner }) => {
-    if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
-    global.db.data.chats[m.chat].isBanned = true
-    m.reply('🗣️ Bot baneado con exito.')
+  const senderNumber = m.sender.replace(/[^0-9]/g, '')
+  const botPath = path.join('./JadiBots', senderNumber)
+
+
+  if (!(isAdmin || isROwner || fs.existsSync(botPath))) {
+    return m.reply('❌ No tienes permisos para usar este comando. Solo admins, owners o subbots pueden hacerlo.')
+  }
+
+  global.db.data.chats[m.chat].isBanned = true
+  m.reply('🍿 Bot baneado con éxito.')
 }
+
 handler.help = ['banearbot']
 handler.tags = ['group']
 handler.command = ['banearbot', 'banchat']
 handler.group = true 
+
 export default handler
