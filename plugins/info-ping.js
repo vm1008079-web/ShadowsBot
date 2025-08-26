e4219c39c9400725da9d23846eff71084d5fa76d
Gian_Pool <116328472+GianPoolS@users.noreply.github.com>
2025-08-26 13:13:06 -0500

    Actualizar info-ping.js

--- plugins/info-ping.js
+++ plugins/info-ping.js
@@ -1,28 +1,56 @@
 import speed from "performance-now";
 import { exec } from "child_process";
+import pkg from '@whiskeysockets/baileys'
+const { proto, generateWAMessageFromContent } = pkg
 
 let handler = async (m, { conn }) => {
   let timestamp = speed();
 
-  exec(`neofetch --stdout`, (error, stdout, stderr) => {
+  exec(`neofetch --stdout`, async (error, stdout, stderr) => {
     let latensi = speed() - timestamp;
     let child = stdout.toString("utf-8");
     let ssd = child.replace(/Memory:/, "Ram:");
 
-    /*const buttons = [
-      {
-        buttonId: `.speed`, // el comando que se ejecutará al hacer clic
-        buttonText: { displayText: "⚡ Ver velocidad" },
-        type: 1
-      }
-    ];*/
+    const textMsg = `${ssd}\n乂  *Speed* : ${latensi.toFixed(4)} _ms_`
 
-    conn.sendMessage(m.chat, {
-      text: `${ssd}\n乂  *Speed* : ${latensi.toFixed(4)} _ms_`,
-      //footer: '📊 Información del sistema',
-      //buttons: buttons,
-      //headerType: 1
-    }, { quoted: m });
+    const buttonsMessage = generateWAMessageFromContent(m.chat, {
+      viewOnceMessage: {
+        message: {
+          interactiveMessage: proto.Message.InteractiveMessage.create({
+            body: proto.Message.InteractiveMessage.Body.create({
+              text: textMsg
+            }),
+            footer: proto.Message.InteractiveMessage.Footer.create({
+              text: '📊 Información del sistema'
+            }),
+            header: proto.Message.InteractiveMessage.Header.create({
+              title: 'SPEED TEST',
+              hasMediaAttachment: false
+            }),
+            nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
+              buttons: [
+                {
+                  name: 'quick_reply',
+                  buttonParamsJson: JSON.stringify({
+                    display_text: '⚡ Ver velocidad',
+                    id: '.speed'
+                  })
+                },
+                {
+                  name: 'quick_reply',
+                  buttonParamsJson: JSON.stringify({
+                    display_text: '📊 Info',
+                    id: '.ping'
+                  })
+                }
+              ]
+            })
+          })
+        }
+      }
+    }, {})
+
+    await conn.relayMessage(m.chat, buttonsMessage.message, { messageId: buttonsMessage.key.id })
   });
 };
 
