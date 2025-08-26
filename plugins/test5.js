d8719aafc68a18321ed981f943d66e786ef0b43a
Gian_Pool <116328472+GianPoolS@users.noreply.github.com>
2025-08-26 11:56:23 -0500

    Actualizar test5.js

---
+++ plugins/test5.js
@@ -0,0 +1,36 @@
+// comando: tes5
+// Requiere @whiskeysockets/baileys
+
+import fs from 'fs'
+
+let handler = async (m, { conn }) => {
+  const destinatario = '51956931649@s.whatsapp.net' // número en privado
+  try {
+    // Carga la imagen desde storage
+    const imagenBuffer = fs.readFileSync('./storage/img/menu.jpg')
+
+    await conn.sendMessage(
+      destinatario,
+      {
+        image: imagenBuffer,
+        caption: 'hola',
+        templateButtons: [
+          { index: 1, quickReplyButton: { displayText: 'Si', id: 'tes5_si' } },
+          { index: 2, quickReplyButton: { displayText: 'No', id: 'tes5_no' } },
+        ],
+        headerType: 4,
+      },
+      { quoted: null } // sin quoted
+    )
+
+    await m.reply('mensaje enviado')
+  } catch (e) {
+    await m.reply(`error: ${e?.message || e}`)
+  }
+}
+
+handler.help = ['tes5']
+handler.tags = ['tools']
+handler.command = /^tes5$/i
+
+export default handler
